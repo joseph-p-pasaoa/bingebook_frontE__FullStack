4a61ca32aee2cca5cc7b2ee8ba04c127e69853af
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-11 20:29:51 -0500

    0.3.2, implement server hostname helper ref file

---
+++ src/helpers/urls.js
@@ -0,0 +1,10 @@
+/*
+JOSEPH P. PASAOA
+URL Reference Helper | Bingebook (a full-stack binge-facilitating app)
+*/
+
+
+const hostname = "http://localhost:11211"; // local server on same machine
+
+
+module.exports = { hostname };
