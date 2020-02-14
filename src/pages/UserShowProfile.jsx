549c79a99f2bbbe806e7d1accfe46ebaa941d6c0
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-13 23:24:03 -0500

    0.9.2, implement return to top link at bottom of usershow profile pages with comments

--- src/pages/UserShowProfile.jsx
+++ src/pages/UserShowProfile.jsx
@@ -6,6 +6,7 @@ UserShowProfile Page Component | Bingebook (a full-stack binge-facilitating app)
 
 /* EXTERNALS - LOCALS */
 import React, { useState, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
@@ -173,6 +174,18 @@ const UserShowProfile = (props) => {
       <ul className="flex-column comments--container">
         {listComments}
       </ul>
+
+      {comments.length > 0
+        ? <Link
+          to={() => false}
+          className="regular-copy return-to-top-link"
+          onClick={(e) => {
+              e.preventDefault();
+              handleReturnToTop();
+          }}
+        >Return to top</Link>
+        : null
+      }
     </>
   );
 }
