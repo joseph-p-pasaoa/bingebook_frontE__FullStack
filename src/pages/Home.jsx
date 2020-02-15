f3d390ed7ff165056d14daace2177f5d9b1c9013
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-15 12:30:37 -0500

    0.13, complete home welcome page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,17 @@ Home Page Component | Bingebook (a full-stack binge-facilitating app)
 import React from 'react';
 // import { connect } from 'react-redux';
 
-// import './Home.css';
+import './Home.css';
 
 
 /* MAIN */
 const Home = () => {
   return (
-    <>
-      Home page
-    </>
+    <div className="home-page">
+      <p className="welcome-msg">What you watch.</p>
+      <p className="welcome-msg">What shows people talk about.<br /><br /></p>
+      <p className="welcome-msg"><em>Mastery</em> over everything you binge.</p>
+    </div>
   );
 }
 
