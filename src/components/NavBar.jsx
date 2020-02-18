33bfcdbcc7a2add5472b2f52c2abbe5a5f456a08
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-17 21:15:34 -0500

    1.1, refactor navBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,40 +8,29 @@ NavBar Component | Bingebook (a full-stack binge-facilitating app)
 import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { NavLink, withRouter } from 'react-router-dom';
+import { NavLink, withRouter, useRouteMatch } from 'react-router-dom';
 
 import './NavBar.css';
 import Logo from './Logo';
 
 
 /* MAIN */
-const NavBar = ({cId, location}) => {
-  let sliderPosition = "translateX(-229px) translateY(0px)";
-  let slideOpacity = 1;
-  // let slideOpacity = 0; // slideOpacity sys commented out for future use
-  switch (location.pathname) {
-    case `/users/${cId}`: 
-        sliderPosition = "translateX(0px)"; break;
-        // slideOpacity = 1; break;
-    case "/users": 
-        sliderPosition = "translateX(154px)"; break;
-        // slideOpacity = 1; break;
-    case `/shows`:
-        sliderPosition = "translateX(308px)"; break;
-        // slideOpacity = 1; break;
-    case `/about`:
-        sliderPosition = "translateX(462px)"; break;
-        // slideOpacity = 1; break;
-    default: break;
-  }
+const NavBar = ({cId}) => {
+  let sliderStyle = { transX: "translateX(0px)", opacity: 0 };
 
-  // navbar slider animation catch for userProfile pages not the current user
-  if (location.pathname.includes("/shows/")) {
-    sliderPosition = "translateX(308px)";
-  } else if (location.pathname.includes("/users/")) {
-    location.pathname.includes(`/users/${cId}`)
-      ? sliderPosition = "translateX(0px)"
-      : sliderPosition = "translateX(154px)";
+  let isCurrentUserRoute = useRouteMatch(`/users/${cId}`);
+  let isUsersRoute = useRouteMatch(`/users`);
+  let isShowsRoute = useRouteMatch(`/shows`);
+  let isAboutRoute = useRouteMatch(`/about`);
+
+  if (isCurrentUserRoute) {
+    sliderStyle = { transX: "translateX(0px)", opacity: 1 };
+  } else if (isUsersRoute) {
+    sliderStyle = { transX: "translateX(154px)", opacity: 1 };
+  } else if (isShowsRoute) {
+    sliderStyle = { transX: "translateX(308px)", opacity: 1 };
+  } else if (isAboutRoute) {
+    sliderStyle = { transX: "translateX(462px)", opacity: 1 };
   }
 
 
@@ -53,7 +42,7 @@ const NavBar = ({cId, location}) => {
         <NavLink to={`/users`} isActive={(match, {pathname}) => pathname.includes("/users/")}><li>Users</li></NavLink>
         <NavLink to={`/shows`}><li>Shows</li></NavLink>
         <NavLink to={`/about`}><li>About</li></NavLink>
-        <li id="active-slide" style={{transform: sliderPosition, opacity: slideOpacity}}></li>
+        <li id="active-slide" style={{transform: sliderStyle.transX, opacity: sliderStyle.opacity}}></li>
       </ul>
     </nav>
   );
