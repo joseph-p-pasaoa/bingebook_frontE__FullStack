23b8c0812c7083ce7973723c8486d104227dedfb
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-17 23:21:21 -0500

    1.2, refactor and organize code better of showcard component

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -14,100 +14,88 @@ const placeholder = require('../assets/images/placeholder-show.svg');
 
 /* MAIN */
 const ShowCard = (props) => {
+  /* MAIN OBJECTIVE IS TO BUILD COMPONENT ACCORDING TO PRESENT ROUTE */
 
+  // CARD SECTION DEFAULTS
   let imgUrl = placeholder;
   if (props.imgUrl && props.imgUrl !== "N/A") {
     imgUrl = props.imgUrl;
   }
-
-  /* CONFIGURE COMPONENT BY PATHNAME */
-  const { pathname } = props.location;
-  let sectionImage = (<img src={imgUrl} alt={`${props.title} poster`} className="card-show--img" />);
-  let sectionTitle = (
-    <h2 className="card-show--title">
-      {props.title}
-    </h2>
+  let sectionImage = (
+        <img src={imgUrl} alt={`${props.title} poster`} className="card-show--img" />
   );
-  let sectionGenres = null,
+  let sectionTitle = (
+        <h2 className="card-show--title">
+          {props.title}
+        </h2>
+  );
+  let sectionYear = (
+        <h3 className="card-show--year">
+          y.{props.year}
+        </h3>
+  );
+  let sectionGenres = (
+        <p className="card--text-single card-show--genres">
+          <strong>Genre(s):</strong> {props.genres}
+        </p>
+  );
+  let sectionImdbLink = (
+        <a className="card--text-single regular-copy"
+          href={`https://www.imdb.com/title/${props.imdbId}/`}
+          target="_blank" rel="noopener noreferrer"
+        >
+          IMDb page
+        </a>
+  );
+  let
     sectionWatchers = null,
     watchStatus = null,
     sectionAddButton = null
   ;
-  const atAddShowPage = useRouteMatch("/users/:id/addShow");
-  const atUserShowPage = useRouteMatch("/shows/:show_id/user/:user_id");
 
-    // ADD SHOW PAGE
 
-  if (atAddShowPage) {
-    sectionGenres = (
-      <p className="card--text-single card-show--genres">
-        <strong>Genre(s):</strong> {props.genres}
-      </p>
-    );
-    if (props.related === false) {
-      sectionAddButton = (
-        <button
-          type="button"
-          onClick={(e) => props.handleAddShowClick(e, props.imdbId)}
-          className="addshow-btn"
-        >
-          Add Show
-        </button>
-      );
-    } else {
-      sectionAddButton = (
-        <button
-          type="button"
-          className="addshow-btn disabled"
-        >
-          Already Binging!
-        </button>
-      );
-    }
-
-    // USER-SHOW RELATIONSHIP PAGE
-
-  } else if (atUserShowPage) {
-    sectionGenres = (
-      <p className="card--text-single card-show--genres">
-        <strong>Genre(s):</strong> {props.genres}
-      </p>
-    );
-
-    // USER PROFILE PAGES
-  } else if (pathname.includes("/user")) {
-    sectionImage = (
+  // CARD SECTION VARIATIONS (dependent on route)
+  const displayAddButton =
+    props.related === true
+      ? (
+          <button
+            type="button"
+            className="addshow-btn disabled"
+          >
+            Already Binging!
+          </button>
+      )
+      : (
+          <button
+            type="button"
+            onClick={(e) => props.handleAddShowClick(e, props.imdbId)}
+            className="addshow-btn"
+          >
+            Add Show
+          </button>
+      )
+  ;
+  const imageLink = (
       <Link to={`/shows/${props.showId}/user/${props.userId}`}>
         <img src={imgUrl} alt={`${props.title} poster`} className="card-show--img" />
       </Link>
-    );
-    sectionTitle = (
+  );
+  const titleLink = (
       <Link to={`/shows/${props.showId}/user/${props.userId}`} className="card-show--title-link">
         <h2 className="card-show--title">
           {props.title}
         </h2>
       </Link>
-    );
-    sectionGenres = (
-      <p className="card--text-single card-show--genres">
-        <strong>Genre(s):</strong> {props.genres}
-      </p>
-    );
-    const describe = {
-      "now": "CURRENTLY BINGING",
-      "onRadar": "ON MY WATCHLIST",
-      "watched": "ANOTHER BINGE COMPLETED!"
-    };
-    watchStatus = (<p className="card--text-single">{`Binge status: ${describe[props.watchStatus]}`}</p>);
-
-    // ALL SHOWS PAGE
-  } else if (pathname.includes("/shows")) {
-    sectionGenres = (
-      <p className="card--text-single card-show--genres">
-        <strong>Genre(s):</strong> {props.genres}
-      </p>
-    );
-    sectionWatchers = (
+  );
+  const describe = {
+    "now": "CURRENTLY BINGING",
+    "onRadar": "ON MY WATCHLIST",
+    "watched": "ANOTHER BINGE COMPLETED!"
+  };
+  const displayStatus = (
+      <p className="card--text-single">{`Binge status: ${ describe[props.watchStatus] }`}</p>
+  );
+  const displayWatchers = (
       <>
         <p className="card--text-single">
           <strong>Who's binging this:</strong>
@@ -116,49 +104,50 @@ const ShowCard = (props) => {
           {props.watchers}
         </ul>
       </>
-    );
+  );
+
+
+  // TOGGLE VARIATIONS BY ROUTE
+  const onAddShowPage = useRouteMatch("/users/:id/addShow");
+  const onUsersRoute = useRouteMatch("/users/:id");
+  const onShowsRoute = useRouteMatch("/shows");
+
+  // for add a show page
+  if (onAddShowPage) {
+    sectionAddButton = displayAddButton;
+
+  // for user profile page
+  } else if (onUsersRoute) {
+    sectionImage = imageLink;
+    sectionTitle = titleLink;
+    watchStatus = displayStatus;
+
+  // for all shows page
+  } else if (onShowsRoute) {
+    sectionWatchers = displayWatchers;
+
   }
 
 
   return (
     <li className="card-show">
 
-      {/* IMAGE */}
       {sectionImage}
 
       <div className="card-show--right-side">
         <div className="card-show--header">
-
-          {/* TITLE */}
           {sectionTitle}
-
-          {/* YEAR */}
-          <h3 className="card-show--year">
-            y.{props.year}
-          </h3>
+          {sectionYear}
         </div>
 
-        {/* GENRES */}
         {sectionGenres}
-
-        {/* IMDB LINK */}
-        <a className="card--text-single regular-copy"
-          href={`https://www.imdb.com/title/${props.imdbId}/`}
-          target="_blank" rel="noopener noreferrer"
-        >
-          IMDb page
-        </a>
-
-        {/* USERS WHO WATCH SHOW */}
-        {sectionWatchers}
-
-        {/* BINGE STATUS */}
-        {watchStatus}
-
-        {/* ADD SHOW BUTTON */}
-        {sectionAddButton}
+        {sectionImdbLink}
+        {sectionWatchers}{/* USERS WHO WATCH SHOW */}
+        {watchStatus}{/* BINGE STATUS */}
+        {sectionAddButton}{/* ADD SHOW BUTTON */}
 
       </div>
+
     </li>
   );
 }
